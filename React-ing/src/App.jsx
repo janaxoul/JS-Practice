081b4d439d007e2a3f373d80330140b535e31d8c
janarthanan <janarthanankofficial@gmail.com>
2025-06-27 16:43:52 +0530

    27th June commit

---
+++ React-ing/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className='main w-full h-screen bg-zinc-900 p-8 text-white font-[Franklin_Gothic_Book]'>
+      <div className='hero w-44 h-32 rounded-xl bg-pink-500 p-4'>Hey my nigga</div>
+    </div>
+  )
+}
+
+export default App
--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-var a=12;
-var b=52;
-console.log("hey "+ (a+b));
-console.log("Hello World");
+
+import { createRoot } from 'react-dom/client';
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
+
+function handleClick(){
+    console.log("React Buttuon clicked");
+}
+const inputField=document.getElementById("myInput");
+inputField.addEventListener("keydown", function(event){
+    console.log("Key pressed: ",event.key);
+});
